9ab8eca3f876b4b9ab54bf54fdff0e36b46e2302
seigneurfuo <seigneurfuo@manjaro-z9>
2022-11-19 23:21:42 +0100

    Ajout de la couleurs dans les messages.

--- easydot.py
+++ easydot.py
@@ -12,6 +12,17 @@ class Messages:
     DST_EXIST_BUT_NO_LINK = "Destination existe mais n'est pas un lien."
     SRC_FOLDER_NOT_FOUND = "Le dossier \"{}\" n'existe pas"
 
+#https://stackoverflow.com/a/287944
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def get_softwares(path):
     ret_list = []
@@ -47,7 +58,6 @@ def browse_tree(path, home_directory=Path().home()):
 
             if os.path.isfile(dst) and not os.path.islink(dst):
                 msg = Messages.DST_EXIST_BUT_NO_LINK
-
             elif not os.path.isfile(dst) and not os.path.islink(dst):
                 msg = Messages.DST_FILE_NOT_EXISTS
             else:
@@ -56,30 +66,38 @@ def browse_tree(path, home_directory=Path().home()):
             yield {"src": src, "dst": dst, "src_short": src_short, "dst_short": dst_short, "msg": msg, "software_name": None}
 
 
-def create_symlinks(files, dry_run):
+def create_symlinks(files, args):
     for filename in files:
         dst_folder = os.path.dirname(filename["dst"])
+
+        if args.force_delete and os.path.isfile(filename["dst"]):
+            if not args.dry:
+                os.remove(filename["dst"])
+
+            msg = "{}[DELETE] {}{}".format(bcolors.WARNING, bcolors.ENDC, filename["dst"])
+            print(msg)
+
         if not os.path.isdir(dst_folder):
-            if not dry_run:
+            if not args.dry:
                 os.makedirs(dst_folder)
 
-            print("    [CREATEFOLDER]", dst_folder)
+            msg = "{}[CREATEFOLDER] {}{}".format(bcolors.OKGREEN, bcolors.ENDC, dst_folder)
+            print(msg)
 
         # Création du lien symbolique
         if not os.path.isfile(filename["dst"]):
-            if not dry_run:
+            if not args.dry:
                 os.symlink(filename["src"], filename["dst"])
 
-            msg = "[+SYMBLINK]"
+            msg = "{}[+SYMBLINK]{}".format(bcolors.OKGREEN, bcolors.ENDC)
 
-        elif os.path.islink(filename["dst"]):
-            msg = "[ERROR: SYMBLINK EXISTS]"
-            # Demander le remplacement ?
-
+        # elif os.path.islink(filename["dst"]):
+        #     msg = "{}[ERROR: SYMBLINK EXISTS]{}".format(bcolors.FAIL, bcolors.ENDC)
+        #     # Demander le remplacement ?
         else:
-            msg = "[ERROR: FILE EXISTS]"
+            msg = "{}[ERROR: FILE EXISTS]{}".format(bcolors.FAIL, bcolors.ENDC)
 
-        print("   ", msg, filename["src"], "->", filename["dst"])
+        print(msg, filename["src"], "->", filename["dst"], "\n")
 
 
 def main():
@@ -87,6 +105,7 @@ def main():
     argument_parser.add_argument("--list", required=False, action="store_true")
     argument_parser.add_argument("--dry", required=False, action="store_true")
     argument_parser.add_argument("--name", type=str, help="Software name")
+    argument_parser.add_argument("--force-delete", required=False, action="store_true")
     args = argument_parser.parse_args()
 
     #current_folder = os.getcwd()
@@ -99,21 +118,15 @@ def main():
             print("    - {}".format(software))
 
     if args.name:
-        if args.dry:
-            dry_run = True
-        else:
-            dry_run = False
+        software_name = args.name.strip().removesuffix("/")
+        folderpath = os.path.join(current_folder, software_name)
 
-        folderpath = os.path.join(current_folder, args.name)
-
-        if args.name in softwares and os.path.isdir(folderpath):
+        if software_name in softwares and os.path.isdir(folderpath):
              files = browse_tree(folderpath)
-             create_symlinks(files, dry_run)
+             create_symlinks(files, args)
         else:
-
             msg = Messages.SRC_FOLDER_NOT_FOUND.format(folderpath)
             print(msg)
-
 
 if __name__ == "__main__":
     main()
